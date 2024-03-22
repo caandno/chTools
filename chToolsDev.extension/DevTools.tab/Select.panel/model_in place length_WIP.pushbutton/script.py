8c6944021e60465996f908746b2abf359ede80d7
caand <68998429+caandno@users.noreply.github.com>
2024-03-22 11:04:13 +0100

    Opprette  model_in_place_length

---
+++ chToolsDev.extension/DevTools.tab/Select.panel/model_in place length_WIP.pushbutton/script.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+__title__ = "model-in place length"
+__doc__ =  """Calculate the lenght of a model in place object and writes i to a length parameter"""
+
+# -----------------Imports--------------------
+from Autodesk.Revit.DB import *
+
+__author__ = "Casper Helmark"
+__helpurl__ = "https://pyrevitlabs.notion.site/pyrevitlabs/pyRevit-bd907d6292ed4ce997c46e84b6ef67a0"
+
+from pyrevit import revit
+
+doc     = __revit__.ActiveUIDocument.Document
+uidoc   = __revit__.ActiveUIDocument
+app     = __revit__.Application
+
+# -----------------Selection--------------------
+selection_ids = uidoc.Selection.GetElementIds()
+
+# -----------------Functions--------------------
+def copy_type_name_to_comments(elements):
+
+    for element in elements:
+        comment = element.get_Parameter(BuiltInParameter.ALL_MODEL_DESCRIPTION.AsValueString())
+        print(comment)
+        # element_type = element.Name
+        # element_name = element_type[(element_type.find(':'))+1:]
+        # comment.Set(element_name)
+
+    print('familie typen {} har fået satt comments til {}'.format(element_type,element_name))
+
+# ---------------------Filter--------------------
+walls   = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Walls).WhereElementIsNotElementType()
+columns = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_StructuralColumns).WhereElementIsNotElementType()
+beams = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_StructuralFraming).WhereElementIsNotElementType()
+
+# -------------------Transaction----------------------
+
+# t= Transaction(doc,__title__)
+# t.Start()
+
+
+copy_type_name_to_comments(selection_ids)
+# copy_type_name_to_comments(columns)
+# copy_type_name_to_comments(beams)
+
+# t.Commit()
